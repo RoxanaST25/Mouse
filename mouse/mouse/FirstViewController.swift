8fc94abf541d8d7f3f7b64d3228213ae623808eb
Roxana Santana <roxana.santana564@gmail.com>
2023-07-10 17:28:41 -0400

    1st commit

---
+++ mouse/mouse/FirstViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstViewController.swift
+//  Social
+//
+//  Created by Main User on 2023-06-26.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ mouse/mouse/NewsTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsTableViewCell.swift
+//  Social
+//
+//  Created by Main User on 2023-06-29.
+//
+
+import UIKit
+import BFWControls
+
+@IBDesignable class NewsTableViewCell: NibTableViewCell {
+
+    
+}
